7b2712d04652bc068d0b50d14f1f74a3b1d6b72b
laboradagrace <grace.laborada@student.passerellesnumeriques.org>
2019-12-10 17:04:12 +0800

    working on mongo

---
+++ BigData_AllProject/src/MongoDB/Col.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MongoDB;
+
+/**
+ *
+ * @author 2ndyrGroupB
+ */
+public class Col {
+    private int col1;
+    private int col2;
+    private int col3;
+    private int col4;
+    private int col5;
+
+    public int getCol1() {
+        return col1;
+    }
+
+    public void setCol1(int col1) {
+        this.col1 = col1;
+    }
+
+    public int getCol2() {
+        return col2;
+    }
+
+    public void setCol2(int col2) {
+        this.col2 = col2;
+    }
+
+    public int getCol3() {
+        return col3;
+    }
+
+    public void setCol3(int col3) {
+        this.col3 = col3;
+    }
+
+    public int getCol4() {
+        return col4;
+    }
+
+    public void setCol4(int col4) {
+        this.col4 = col4;
+    }
+
+    public int getCol5() {
+        return col5;
+    }
+
+    public void setCol5(int col5) {
+        this.col5 = col5;
+    }
+    
+         
+}
